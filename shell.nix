b7a5a79efec74303dfca5a5f95eef9ccccdfa318
Dan Dart <git@dandart.co.uk>
2023-08-10 20:16:17 +0100

    use cached packages

--- shell.nix
+++ shell.nix
@@ -33,11 +33,11 @@ runCommand "danos" {
     export AS_X86_64=x86_64-unknown-linux-gnu-as
     export OBJCOPY_X86_64=x86_64-unknown-linux-gnu-objcopy
     export STRIP_X86_64=x86_64-unknown-linux-gnu-strip
-    export CC_X86_32=i686-elf-gcc
-    export LD_X86_32=i686-elf-ld
-    export AS_X86_32=i686-elf-as
-    export OBJCOPY_X86_32=i686-elf-objcopy
-    export STRIP_X86_32=i686-elf-strip
+    export CC_X86_32=i686-unknown-linux-gnu-gcc
+    export LD_X86_32=i686-unknown-linux-gnu-ld
+    export AS_X86_32=i686-unknown-linux-gnu-as
+    export OBJCOPY_X86_32=i686-unknown-linux-gnu-objcopy
+    export STRIP_X86_32=i686-unknown-linux-gnu-strip
     export CC_ARM=arm-none-eabi-gcc
     export LD_ARM=arm-none-eabi-ld
     export AS_ARM=arm-none-eabi-as
@@ -48,11 +48,11 @@ runCommand "danos" {
     export AS_ARMHF=arm-none-eabihf-as
     export OBJCOPY_ARMHF=arm-none-eabihf-objcopy
     export STRIP_ARMHF=arm-none-eabihf-strip
-    export CC_AARCH64=aarch64-none-elf-gcc
-    export LD_AARCH64=aarch64-none-elf-ld
-    export AS_AARCH64=aarch64-none-elf-as
-    export OBJCOPY_AARCH64=aarch64-none-elf-objcopy
-    export STRIP_AARCH64=aarch64-none-elf-strip
+    export CC_AARCH64=gcc
+    export LD_AARCH64=ld
+    export AS_AARCH64=as
+    export OBJCOPY_AARCH64=objcopy
+    export STRIP_AARCH64=strip
     '' else ''
     [ -f OVMF_VARS.fd ] || cp ${OVMFFull.fd.outPath}/FV/OVMF_CODE.fd .
     [ -f OVMF_VARS.fd ] || cp ${OVMFFull.fd.outPath}/FV/OVMF_VARS.fd .
@@ -96,12 +96,13 @@ runCommand "danos" {
         pkgsCross.arm-embedded.pkgsBuildHost.gcc
         pkgsCross.armhf-embedded.pkgsBuildHost.gcc
     ] else (if builtins.currentSystem == "aarch64-linux" then [
-        pkgsCross.i686-embedded.pkgsBuildHost.gcc
-        pkgsCross.x86_64-embedded.pkgsBuildHost.gcc
+        pkgsCross.gnu32.pkgsBuildHost.gcc
+        # pkgsCross.x86_64-embedded.pkgsBuildHost.gcc # not cached
         pkgsCross.gnu64.pkgsBuildHost.gcc
-        pkgsCross.aarch64-embedded.pkgsBuildHost.gcc
+        # pkgsCross.aarch64-embedded.pkgsBuildHost.gcc # not cached
         pkgsCross.arm-embedded.pkgsBuildHost.gcc
-        pkgsCross.armhf-embedded.pkgsBuildHost.gcc
+        # pkgsCross.armhf-embedded.pkgsBuildHost.gcc # not cached
+        pkgsCross.armv7l-hf-multiplatform.pkgsBuildHost.gcc
         pkgsCross.gnu64.pkgsHostTarget.gnu-efi
     ] else [
         gnu-efi
